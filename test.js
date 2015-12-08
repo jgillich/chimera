63fdb0f261e36e6a6fa1a7dd76d9b4b185857661
Jakob Gillich <jakob@gillich.me>
2015-12-08 01:37:43 +0100

    proper tests

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 var test = require('tape');
+var fs = require('fs');
 
-test('self test', function(t) {
-  t.plan(1);
-  t.equal(true, true);
+test('env', function(t) {
+  t.equal(process.env.TEST_ALL, 'true');
+  t.equal(process.env.TEST_ENV_TARGET, process.env.CHIMERA_TARGET);
+});
+
+test('install', function(t) {
+  t.doesNotThrow(fs.statSync.bind(fs, '/test-install-all'));
+  t.doesNotThrow(fs.statSync.bind(fs, '/test-install-target'));
 });
